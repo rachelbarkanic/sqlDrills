1f8e0b5e31e93d2530def4d83f4b9ac063319f04
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-10-13 17:42:07 -0700

    artist done

---
+++ artist.sql
@@ -0,0 +1,22 @@
+1. INSERT INTO artist (name)
+VALUES ('Taking Back Sunday')
+
+INSERT INTO artist (name)
+VALUES ('Timber Timbre')
+
+INSERT INTO artist (name)
+VALUES ('Chicano Batman')
+
+INSERT INTO artist (name)
+VALUES ('Pond')
+
+INSERT INTO artist (name)
+VALUES ('blink-182')
+
+2. SELECT * FROM artist ORDER BY name DESC LIMIT 10;
+
+3. SELECT * FROM artist ORDER BY name ASC LIMIT 5; 
+
+4. SELECT * FROM artist WHERE name LIKE 'Black%';
+
+5. SELECT * FROM artist WHERE name LIKE '%BLACK%';
